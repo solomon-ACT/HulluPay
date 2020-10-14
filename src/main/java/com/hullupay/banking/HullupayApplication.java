3f1317ccc191ebe2e3d228f9bfe6f76d894ec945
jonmersha@gmail.com <you@example.com>
2020-10-14 18:14:16 +0300

    test 3

--- src/main/java/com/hullupay/banking/HullupayApplication.java
+++ src/main/java/com/hullupay/banking/HullupayApplication.java
@@ -16,7 +16,7 @@ public class HullupayApplication {
 	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
+	return "Hello Well come to Hullupay Sysyem";
 	}
 
 }

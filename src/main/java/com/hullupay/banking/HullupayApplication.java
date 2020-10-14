e3995d17932cf11bcce1e33878881792b08aeaeb
yohannes <jonmersha@gmail.com>
2020-10-14 11:55:46 +0300

    first commit

---
+++ src/main/java/com/hullupay/banking/HullupayApplication.java
@@ -0,0 +1,13 @@
+package com.hullupay.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HullupayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HullupayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hullupay/banking/HullupayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hullupay.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HullupayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
